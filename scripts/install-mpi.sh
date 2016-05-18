e2c9ace4cacbba343dabfa8231316b7cfb7f6490
Matt Spraggs <matthew.spraggs@gmail.com>
2016-05-18 13:59:05 -0400

    Trying out openmpi addon for clang

---
+++ scripts/install-mpi.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+MPI_IMPL="$1"
+
+case "$MPI_IMPL" in
+  openmpi16)
+    if [ ! -d "$HOME/.local/$MPI_IMPL/bin" ]
+    then
+      echo "Installing OpenMPI 1.6.5"
+      wget --no-check-certificate https://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.5.tar.gz
+      tar xf openmpi-1.6.5.tar.gz
+      mkdir -p openmpi-1.6.5/build
+      cd openmpi-1.6.5/build
+      ../configure --prefix="$HOME/.local/$MPI_IMPL" > /dev/null 2>&1
+      make > /dev/null 2>&1
+      make install > /dev/null 2>&1
+      cd ../..
+    else
+      echo "Using cached OpenMPI 1.6.5"
+    fi
+    ;;
+  *)
+    echo "Unknown MPI implementation"
+esac
+
+export PATH=$PATH:$HOME/.local/$MPI_IMPL/bin
+export LIBRARY_PATH=$LIBRARY_PATH:$HOME/.local/$MPI_IMPL/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/$MPI_IMPL/lib
+export CC=mpicc
+export CXX=mpicxx
